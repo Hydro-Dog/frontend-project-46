9fdb6ecc30c3e986022b4b2686a524180c78e6aa
Vlad Babaev <vladbabaev11@gmail.com>
2022-11-22 21:57:15 +0300

    stylish working draft 2

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -2,35 +2,35 @@ import extraTypeOf from '../utils/extraTypeOf.js';
 import calcSpaces from '../utils/calcSpaces.js';
 
 
-const getSpaces = (length) => Array.from({ length }, () => '=').join('');
+const getSpaces = (length) => Array.from({ length }, () => ' ').join('');
 
 const prettifyValue = (value, spaces = 0) => {
   if (extraTypeOf(value) === 'object') {
-    return Object.entries(value).map(([key, val]) => `\n    ${key}: ${val}\n`).join('');
+    return Object.entries(value).map(([key, val]) => `\n${getSpaces(spaces + 2)}${key}: ${prettifyValue(val, spaces + 2)}`).join('');
   }
   return value;
 };
 
 const stylish = (tree, spaces = 0) => tree.children.map((item) => {
   if (item.type === 'tree') {
-    return `${item.key}: \n${stylish(item)}`;
+    return `  ${getSpaces(spaces)}${item.key}: \n${stylish(item, spaces + 2)}`;
   }
 
   if (item.type === 'leaf') {
     if (item.status === 'added') {
-      return ` + ${item.key}: ${prettifyValue(item.value)} \n`;
+      return `${getSpaces(spaces)}+ ${item.key}: ${prettifyValue(item.value)} \n`;
     }
 
     if (item.status === 'removed') {
-      return ` - ${item.key}: ${prettifyValue(item.value)} \n`;
+      return `${getSpaces(spaces)}- ${item.key}: ${prettifyValue(item.value)} \n`;
     }
 
     if (item.status === 'updated') {
-      return ` - ${item.key}: ${prettifyValue(item.prevValue)}\n + ${item.key}: ${prettifyValue(item.newValue)} \n`;
+      return `${getSpaces(spaces)}- ${item.key}: ${prettifyValue(item.prevValue)}\n${getSpaces(spaces)}+ ${item.key}: ${prettifyValue(item.newValue)} \n`;
     }
 
     if (item.status === 'same') {
-      return `   ${item.key}: ${prettifyValue(item.value)} \n`;
+      return `${getSpaces(spaces)}  ${item.key}: ${prettifyValue(item.value)} \n`;
     }
   }
 }).join('');
