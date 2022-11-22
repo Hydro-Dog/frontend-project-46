9c09cb6db8ed93c21794b0b8393dd9c97d8ba3de
Vlad Babaev <vladbabaev11@gmail.com>
2022-11-22 22:31:12 +0300

    stylish finished looks like

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,36 +1,35 @@
 import extraTypeOf from '../utils/extraTypeOf.js';
 import calcSpaces from '../utils/calcSpaces.js';
 
-
 const getSpaces = (length) => Array.from({ length }, () => ' ').join('');
 
 const prettifyValue = (value, spaces = 0) => {
   if (extraTypeOf(value) === 'object') {
-    return Object.entries(value).map(([key, val]) => `\n${getSpaces(spaces + 2)}${key}: ${prettifyValue(val, spaces + 2)}`).join('');
+    return Object.entries(value).map(([key, val]) => `\n${getSpaces(spaces + 2)}${key}: ${prettifyValue(val, spaces + 4)}`).join('');
   }
   return value;
 };
 
 const stylish = (tree, spaces = 0) => tree.children.map((item) => {
   if (item.type === 'tree') {
-    return `  ${getSpaces(spaces)}${item.key}: \n${stylish(item, spaces + 2)}`;
+    return `  ${getSpaces(spaces)}${item.key}: \n${stylish(item, spaces + 4)}`;
   }
 
   if (item.type === 'leaf') {
     if (item.status === 'added') {
-      return `${getSpaces(spaces)}+ ${item.key}: ${prettifyValue(item.value)} \n`;
+      return `${getSpaces(spaces)}+ ${item.key}: ${prettifyValue(item.value, spaces + 4)} \n`;
     }
 
     if (item.status === 'removed') {
-      return `${getSpaces(spaces)}- ${item.key}: ${prettifyValue(item.value)} \n`;
+      return `${getSpaces(spaces)}- ${item.key}: ${prettifyValue(item.value, spaces + 4)} \n`;
     }
 
     if (item.status === 'updated') {
-      return `${getSpaces(spaces)}- ${item.key}: ${prettifyValue(item.prevValue)}\n${getSpaces(spaces)}+ ${item.key}: ${prettifyValue(item.newValue)} \n`;
+      return `${getSpaces(spaces)}- ${item.key}: ${prettifyValue(item.prevValue, spaces + 4)}\n${getSpaces(spaces)}+ ${item.key}: ${prettifyValue(item.newValue, spaces + 4)} \n`;
     }
 
     if (item.status === 'same') {
-      return `${getSpaces(spaces)}  ${item.key}: ${prettifyValue(item.value)} \n`;
+      return `${getSpaces(spaces)}  ${item.key}: ${prettifyValue(item.value, spaces + 4)} \n`;
     }
   }
 }).join('');
