a04ac0dd2a82ca718e60f7ae845032cc3c3e4daa
Vlad Babaev <vladbabaev11@gmail.com>
2022-11-23 19:06:22 +0300

    plain finished

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -5,14 +5,14 @@ const getSpaces = (length) => Array.from({ length }, () => ' ').join('');
 
 const prettifyValue = (value, spaces = 0) => {
   if (extraTypeOf(value) === 'object') {
-    return Object.entries(value).map(([key, val]) => `\n${getSpaces(spaces + 2)}${key}: ${prettifyValue(val, spaces + 4)}`).join('');
+    return '{' + Object.entries(value).map(([key, val]) => `\n${getSpaces(spaces + 2)}${key}: ${prettifyValue(val, spaces + 4)}`).join('') + `\n${getSpaces(spaces - 2)}}`;
   }
   return value;
 };
 
 const stylish = (tree, spaces = 0) => tree.children.map((item) => {
   if (item.type === 'tree') {
-    return `  ${getSpaces(spaces)}${item.key}: \n${stylish(item, spaces + 4)}`;
+    return `  ${getSpaces(spaces)}${item.key}: {\n${stylish(item, spaces + 4)}${getSpaces(spaces+2)}}\n`;
   }
 
   if (item.type === 'leaf') {
