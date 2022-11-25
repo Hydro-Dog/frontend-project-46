746d646825697e9b68a35f0c6dc3ede2afc851d8
Vlad Babaev <vladbabaev11@gmail.com>
2022-11-25 14:30:40 +0300

    push removed

--- src/utils/generateDiffTree.js
+++ src/utils/generateDiffTree.js
@@ -4,41 +4,41 @@ const generateDiffTree = (obj1, obj2, key = null) => {
   const keysSet = new Set([...Object.keys(obj1), ...Object.keys(obj2)]);
   const keys = Array.from(keysSet);
 
-  const result = [];
+  let result = [];
 
   keys.forEach((item) => {
     if (extraTypeOf(obj1[item]) === 'object' && extraTypeOf(obj2[item]) === 'object') {
       const treeResult = generateDiffTree(obj1[item], obj2[item], item);
-      result.push(treeResult);
+      result = [...result, treeResult];
     } else if (obj1[item] === obj2[item] && obj1[item] !== undefined && obj2[item] !== undefined) {
-      result.push({
+      result = [...result, {
         key: item,
         status: 'same',
         value: obj1[item],
         type: 'leaf',
-      });
+      }];
     } else if (obj1[item] !== obj2[item] && obj1[item] !== undefined && obj2[item] !== undefined) {
-      result.push({
+      result = [...result, {
         key: item,
         status: 'updated',
         prevValue: obj1[item],
         newValue: obj2[item],
         type: 'leaf',
-      });
+      }];
     } else if (obj1[item] === undefined) {
-      result.push({
+      result = [...result, {
         key: item,
         status: 'added',
         value: obj2[item],
         type: 'leaf',
-      });
+      }];
     } else if (obj2[item] === undefined) {
-      result.push({
+      result = [...result, {
         key: item,
         status: 'removed',
         value: obj1[item],
         type: 'leaf',
-      });
+      }];
     }
   });
 
