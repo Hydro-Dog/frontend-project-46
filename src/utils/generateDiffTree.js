04d052cea87628b086853bee8bd52bf69b58dbc1
Vlad Babaev <vladbabaev11@gmail.com>
2022-11-25 15:06:56 +0300

    mutations removed

--- src/utils/generateDiffTree.js
+++ src/utils/generateDiffTree.js
@@ -4,57 +4,48 @@ const generateDiffTree = (obj1, obj2, key = null) => {
   const keysSet = new Set([...Object.keys(obj1), ...Object.keys(obj2)]);
   const keys = Array.from(keysSet);
 
-  let result = [];
-
-  keys.forEach((item) => {
+  const result = keys.map((item) => {
     if (extraTypeOf(obj1[item]) === 'object' && extraTypeOf(obj2[item]) === 'object') {
-      const treeResult = generateDiffTree(obj1[item], obj2[item], item);
-      result = [...result, treeResult];
-    } else if (obj1[item] === obj2[item] && obj1[item] !== undefined && obj2[item] !== undefined) {
-      result = [...result, {
+      return generateDiffTree(obj1[item], obj2[item], item);
+    } if (obj1[item] === obj2[item] && obj1[item] !== undefined && obj2[item] !== undefined) {
+      return {
         key: item,
         status: 'same',
         value: obj1[item],
         type: 'leaf',
-      }];
-    } else if (obj1[item] !== obj2[item] && obj1[item] !== undefined && obj2[item] !== undefined) {
-      result = [...result, {
+      };
+    } if (obj1[item] !== obj2[item] && obj1[item] !== undefined && obj2[item] !== undefined) {
+      return {
         key: item,
         status: 'updated',
         prevValue: obj1[item],
         newValue: obj2[item],
         type: 'leaf',
-      }];
-    } else if (obj1[item] === undefined) {
-      result = [...result, {
+      };
+    } if (obj1[item] === undefined) {
+      return {
         key: item,
         status: 'added',
         value: obj2[item],
         type: 'leaf',
-      }];
-    } else if (obj2[item] === undefined) {
-      result = [...result, {
+      };
+    } if (obj2[item] === undefined) {
+      return {
         key: item,
         status: 'removed',
         value: obj1[item],
         type: 'leaf',
-      }];
+      };
     }
+
+    return null;
   });
 
   return {
     key,
     type: 'tree',
     status: 'same',
-    children: result.sort((item1, item2) => {
-      if (item1.key < item2.key) {
-        return -1;
-      }
-      if (item1.key > item2.key) {
-        return 1;
-      }
-      return 0;
-    }),
+    children: result,
   };
 };
 
