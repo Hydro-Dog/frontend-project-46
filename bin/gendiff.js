32823c7fb1ce3e924ab86b01ce1f3fabf02e42d1
Vlad Babaev <vladbabaev11@gmail.com>
2022-11-25 13:01:10 +0300

    main changed back

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -10,7 +10,6 @@ program
 
 program
   .helpOption('-h, --help', 'output usage information')
-  .option('-V, --version', 'output the version number')
   .option('-f, --format <type>', 'output format', 'stylish');
 
 program
