79c37d31ca40b7b4476ff16081a36959178f84a7
Vlad Babaev <vladbabaev11@gmail.com>
2022-12-02 17:01:14 +0300

    depth 1

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,37 +1,37 @@
 import _ from 'lodash';
 
-const getSpaces = (depth) => Array.from({ length: depth }, () => ' ').join('');
+const getSpaces = (depth) => Array.from({ length: depth * 2 }, () => ' ').join('');
 
-const prettifyValue = (value, depth = 0) => {
+const prettifyValue = (value, depth) => {
   if (_.isPlainObject(value)) {
-    return `{${Object.entries(value).map(([key, val]) => `\n${getSpaces(depth + 2)}${key}: ${prettifyValue(val, depth + 4)}`).join('')}\n${getSpaces(depth - 2)}}`;
+    return `{${Object.entries(value).map(([key, val]) => `\n${getSpaces(depth + 1)}${key}: ${prettifyValue(val, depth + 2)}`).join('')}\n${getSpaces(depth - 1)}}`;
   }
   return value;
 };
 
-const getChildren = (tree, depth = 0) => tree.children.map((item) => {
+const getStylishOutput = (tree, depth) => tree.children.map((item) => {
   if (item.status === 'tree') {
-    return `  ${getSpaces(depth)}${item.key}: {\n${getChildren(item, depth + 4)}${getSpaces(depth + 2)}}\n`;
+    return `  ${getSpaces(depth)}${item.key}: {\n${getStylishOutput(item, depth + 2)}${getSpaces(depth + 1)}}\n`;
   }
   if (item.status === 'added') {
-    return `${getSpaces(depth)}+ ${item.key}: ${prettifyValue(item.value, depth + 4)}\n`;
+    return `${getSpaces(depth)}+ ${item.key}: ${prettifyValue(item.value, depth + 2)}\n`;
   }
 
   if (item.status === 'removed') {
-    return `${getSpaces(depth)}- ${item.key}: ${prettifyValue(item.value, depth + 4)}\n`;
+    return `${getSpaces(depth)}- ${item.key}: ${prettifyValue(item.value, depth + 2)}\n`;
   }
 
   if (item.status === 'updated') {
-    return `${getSpaces(depth)}- ${item.key}: ${prettifyValue(item.prevValue, depth + 4)}\n${getSpaces(depth)}+ ${item.key}: ${prettifyValue(item.newValue, depth + 4)}\n`;
+    return `${getSpaces(depth)}- ${item.key}: ${prettifyValue(item.prevValue, depth + 2)}\n${getSpaces(depth)}+ ${item.key}: ${prettifyValue(item.newValue, depth + 2)}\n`;
   }
 
   if (item.status === 'same') {
-    return `${getSpaces(depth)}  ${item.key}: ${prettifyValue(item.value, depth + 4)}\n`;
+    return `${getSpaces(depth)}  ${item.key}: ${prettifyValue(item.value, depth + 2)}\n`;
   }
 
   return null;
 }).join('');
 
-const stylish = (tree) => `{\n${getChildren(tree, 2)}}`;
+const stylish = (tree) => `{\n${getStylishOutput(tree, 1)}}`;
 
 export default stylish;
